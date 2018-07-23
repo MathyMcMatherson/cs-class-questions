d87e08893d90970796cc91de2dacfc0cab93cdae
Dan <dschneider@amphi.com>
2018-07-22 17:23:53 -0700

    Able to server static page

--- server.js
+++ server.js
@@ -10,12 +10,25 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 mongoose.connect('mongodb://dschneider:SmellyS0ckMan@ds243491.mlab.com:43491/questions_test');     // connect to mongoDB database on modulus.io
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public_html'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+
+
+
+
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+    res.sendfile('./public_html/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+
+
+
+
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
